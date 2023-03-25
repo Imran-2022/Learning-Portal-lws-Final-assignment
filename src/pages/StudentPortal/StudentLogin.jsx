6f1c68426ef5ed955db1e1f7998b14df2a91af1f
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-03-25 12:17:56 +0600

    implement login & register

--- src/pages/StudentPortal/StudentLogin.jsx
+++ src/pages/StudentPortal/StudentLogin.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import lwsL from '../../assets/image/learningportal.svg'
+import { useLoginMutation } from '../../features/auth/authApi';
 
 const StudentLogin = () => {
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const [login, { data, isLoading, error: responseError }] =
+        useLoginMutation();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (responseError?.data) {
+            setError(responseError.data);
+        }
+        if (data?.accessToken && data?.user) {
+            navigate("/");
+        }
+    }, [data, responseError, navigate]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        login({
+            email,
+            password,
+        });
+    };
+
+
     return (
         <div>
             <section className="py-6 bg-primary h-screen grid place-items-center">
@@ -12,18 +45,22 @@ const StudentLogin = () => {
                             Sign in to Student Account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
+                    <form className="mt-8 space-y-6" action="#" onSubmit={handleSubmit}>
                         <input type="hidden" name="remember" value="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="email-address" className="sr-only">Email address</label>
                                 <input id="email-address" name="email" type="email" autoComplete="email" required
-                                    className="login-input rounded-t-md" placeholder="Email address" />
+                                    className="login-input rounded-t-md" placeholder="Email address" value={email}
+                                    onChange={(e) => setEmail(e.target.value)}/>
                             </div>
                             <div>
                                 <label htmlFor="password" className="sr-only">Password</label>
                                 <input id="password" name="password" type="password" autoComplete="current-password" required
-                                    className="login-input rounded-b-md" placeholder="Password" />
+                                    className="login-input rounded-b-md" placeholder="Password" value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }/>
                             </div>
                         </div>
 
@@ -36,7 +73,7 @@ const StudentLogin = () => {
                         </div>
 
                         <div>
-                            <button type="submit"
+                            <button type="submit" disabled={isLoading}
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
                                 Sign in
                             </button>
--- src/pages/StudentPortal/StudentRegistration.jsx
+++ src/pages/StudentPortal/StudentRegistration.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import lwsL from '../../assets/image/learningportal.svg'
+import { useRegisterMutation } from '../../features/auth/authApi';
 
 const StudentRegistration = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [agreed, setAgreed] = useState(false);
+    const [error, setError] = useState("");
+
+    const [register, { data, isLoading, error: responseError }] =
+        useRegisterMutation();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (responseError?.data) {
+            setError(responseError.data);
+        }
+        if (data?.accessToken && data?.user) {
+            navigate("/");
+        }
+    }, [data, responseError, navigate]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        if (confirmPassword !== password) {
+            setError("Passwords do not match");
+        } else {
+            register({
+                name,
+                email,
+                password,
+            });
+        }
+    };
+
     return (
         <div>
             <section className="py-6 bg-primary h-screen grid place-items-center">
@@ -12,29 +52,45 @@ const StudentRegistration = () => {
                             Create Your New Account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
+                    <form className="mt-8 space-y-6" action="#" onSubmit={handleSubmit}>
                         <input type="hidden" name="remember" value="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="name" className="sr-only">Name</label>
                                 <input id="name" name="name" type="name" autoComplete="name" required
-                                    className="login-input rounded-t-md" placeholder="Student Name" />
+                                    className="login-input rounded-t-md" placeholder="Student Name" 
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    />
                             </div>
                             <div>
                                 <label htmlFor="email-address" className="sr-only">Email address</label>
                                 <input id="email-address" name="email" type="email" autoComplete="email" required
-                                    className="login-input " placeholder="Email address" />
+                                    className="login-input " placeholder="Email address" 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    />
                             </div>
                             <div>
                                 <label htmlFor="password" className="sr-only">Password</label>
                                 <input id="password" name="password" type="password" autoComplete="current-password" required
-                                    className="login-input" placeholder="Password" />
+                                    className="login-input" placeholder="Password" 
+                                    value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                    />
                             </div>
                             <div>
                                 <label htmlFor="confirm-password" className="sr-only">Confirm Password</label>
                                 <input id="confirm-password" name="confirm-password" type="password"
                                     autoComplete="confirm-password" required className="login-input rounded-b-md"
-                                    placeholder="Confirm Password" />
+                                    placeholder="Confirm Password" 
+                                    value={confirmPassword}
+                                    onChange={(e) =>
+                                        setConfirmPassword(e.target.value)
+                                    }
+                                    />
                             </div>
                         </div>
 
