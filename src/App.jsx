353660db0b476c967cadacafb56f74f7caa077a8
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-03-26 15:08:18 +0600

    work on admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { AdminLogin, Assignments, AssignmentsMark, CoursePlayer, DashBoard, LeaderBorad, Quiz, Quizzes, StudentLogin, StudentRegistration, Videos } from './AllRoutes'
 import Navigation from './components/Navigation'
+import useAuthCheck from './hooks/useAuthCheck'
+
 function App() {
-  return (
+
+  const authChecked = useAuthCheck();
+
+
+  return !authChecked ? <div className="flex bg-white gap-3 justify-center items-center h-screen font-bold">
+    <p className="text-cyan-700">Auth Checking ....</p>
+    <div className="w-12 h-12 rounded-full animate-spin border-x-2 border-solid border-blue-500 border-t-transparent"></div>
+  </div> : (
     <div>
       <Routes>
         <Route path='/' element={<StudentLogin />} />
--- src/pages/Dashboard/Videos.jsx
+++ src/pages/Dashboard/Videos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import lwsL from '../../assets/image/learningportal.svg'
+import AdminNav from '../../components/AdminNav';
 import Video from '../../components/Video';
 import { useGetVideosQuery } from '../../features/adminPortal/videos/videosApi';
 
@@ -15,24 +15,7 @@ const Videos = () => {
     if (!isLoading && !isError && videos?.length > 0) content = videos.map(dt => <Video key={dt.id} dt={dt} />)
     return (
         <div>
-            <nav className="shadow-md">
-                <div className="max-w-7xl px-5 lg:px-0 mx-auto flex justify-between py-3">
-                    <img className="h-10" src={lwsL} />
-                    <div className="flex items-center gap-3">
-                        <h2 className="font-bold">Admin</h2>
-                        <button
-                            className="flex gap-2 items-center px-4 py-1 rounded-full text-sm transition-all bg-red-600 hover:bg-red-700 font-medium">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                            </svg>
-                            Logout
-                        </button>
-                    </div>
-                </div>
-            </nav>
-
+          <AdminNav/>
             <section className="py-6 bg-primary">
                 <div className="mx-auto max-w-full px-5 lg:px-20">
                     <div className="px-3 py-20 bg-opacity-10">
